26c751f3dfeb704f0eb701faff8b2bd0c2e7f201
seyyed hassan moosavi nazar <73283752+moosavinazar@users.noreply.github.com>
2021-12-15 22:39:09 +0330

    use of, from and user httpClient to get date from mockData.json file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {of, from} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'observables3';
+export class AppComponent implements OnInit {
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit(): void {
+
+    this.http.get('assets/mockData.json').subscribe(console.log);
+
+    of(['name1', 'name2', 'name3']).subscribe(console.log);
+
+    of('name1', 'name2', 'name3').subscribe(console.log);
+    from(['name1', 'name2', 'name3']).subscribe(console.log);
+
+  }
 }
